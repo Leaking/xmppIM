1c2ed544e22cd2d0634f0c542eab78dcce9efad3
Chen <chenhuazhaoao@gmail.com>
2014-04-13 13:51:01 +0800

    change：finish the contacts list,including the title and the items

--- src/com/XMPP/mainview/ContactsFragment.java
+++ src/com/XMPP/mainview/ContactsFragment.java
@@ -1,19 +1,134 @@
 package com.XMPP.mainview;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.XMPP.R;
+import com.XMPP.R.color;
+
+import com.atermenji.android.iconicdroid.IconicFontDrawable;
+import com.atermenji.android.iconicdroid.icon.EntypoIcon;
+
 
 public class ContactsFragment extends Fragment {
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_contacts, container, false);
+		View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        ExpandableListView expandableListView = (ExpandableListView)view.findViewById(R.id.contactExpandableList);  
+        expandableListView.setAdapter(expandAdapter);  
+        return view;
 	}
+	
+	
+	
+	ExpandableListAdapter expandAdapter = new BaseExpandableListAdapter(){
+    	//names of the groups
+        private String[] groups = new String[] { "我的好友", "陌生人", "炮友","黑名单" };  
+		//names of the items
+        private String[][] items = new String[][]
+		{
+			{ "毛爷爷", "邓爷爷", "德玛西亚", "寒冰射手" },
+			{ "梅西", "罗纳尔多", "卡卡", "奥巴马" },
+			{ "苍老师", "小泽玛利亚" , "啦啦啦" },
+			{ "薄熙来","周永康"}
+		};
+      //自己定义一个获得文字信息的方法  
+//        TextView getTextView() {  
+//            AbsListView.LayoutParams lp = new AbsListView.LayoutParams(  
+//                    ViewGroup.LayoutParams.MATCH_PARENT, 10);  
+//            TextView textView = new TextView(MainActivity.this);  
+//            textView.setLayoutParams(lp);  
+//            textView.setGravity(Gravity.CENTER_VERTICAL);  
+//            textView.setPadding(36, 0, 0, 0);  
+//            textView.setTextSize(40);  
+//            textView.setTextColor(Color.BLACK);  
+//            return textView;  
+//        }  
+
+          
+        //重写ExpandableListAdapter中的各个方法  
+        @Override  
+        public int getGroupCount() {  
+            return groups.length;  
+        }  
+
+        @Override  
+        public Object getGroup(int groupPosition) {  
+            return groups[groupPosition];  
+        }  
+
+        @Override  
+        public long getGroupId(int groupPosition) {  
+            return groupPosition;  
+        }  
+
+        @Override  
+        public int getChildrenCount(int groupPosition) {  
+            return items[groupPosition].length;  
+        }  
+
+        @Override  
+        public Object getChild(int groupPosition, int childPosition) {  
+            return items[groupPosition][childPosition];  
+        }  
+
+        @Override  
+        public long getChildId(int groupPosition, int childPosition) {  
+            return childPosition;  
+        }  
+
+        @Override  
+        public boolean hasStableIds() {  
+            return true;  
+        }  
+
+        @Override  
+        public View getGroupView(int groupPosition, boolean isExpanded,  
+                View convertView, ViewGroup parent) {  
+            LinearLayout ll = (LinearLayout)View.inflate(ContactsFragment.this.getActivity(),R.layout.expand_list_title,null);           
+            ImageView arrowImage= (ImageView)ll.findViewById(R.id.listTitle_arrow);
+            TextView groupName = (TextView)ll.findViewById(R.id.listTitle_groupname);
+            groupName.setText(getGroup(groupPosition).toString());
+            IconicFontDrawable iconicFontDrawable = new IconicFontDrawable(ContactsFragment.this.getActivity());
+            if(!isExpanded){
+                iconicFontDrawable.setIcon(EntypoIcon.CHEVRON_THIN_RIGHT);
+                iconicFontDrawable.setIconColor(com.XMPP.R.color.pocket_gold);
+            }else{
+            	iconicFontDrawable.setIcon(EntypoIcon.CHEVRON_THIN_DOWN);
+            	iconicFontDrawable.setIconColor(com.XMPP.R.color.pocket_gold);
+            }
+            arrowImage.setBackground(iconicFontDrawable);            
+            return ll;  
+        }  
+
+        @Override  
+        public View getChildView(int groupPosition, int childPosition,  
+                boolean isLastChild, View convertView, ViewGroup parent) {  
+            LinearLayout ll = (LinearLayout)View.inflate(ContactsFragment.this.getActivity(),R.layout.expand_list_item,null);           
+            TextView itemName = (TextView)ll.findViewById(R.id.itemName);
+            itemName.setText("德玛西亚");
+            
+            
+            return ll;  
+        }  
+
+        @Override  
+        public boolean isChildSelectable(int groupPosition,int childPosition) {  
+            return true;  
+        }  
+
+    };  
 }
--- src/com/XMPP/mainview/MainviewActivity.java
+++ src/com/XMPP/mainview/MainviewActivity.java
@@ -1,14 +1,18 @@
 package com.XMPP.mainview;
 
 
+import java.util.Random;
+
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,45 +25,49 @@ import com.XMPP.smack.Smack;
 import com.atermenji.android.iconicdroid.IconicFontDrawable;
 import com.atermenji.android.iconicdroid.icon.IconicIcon;
 
-public class MainviewActivity extends FragmentActivity implements OnClickListener {
+public class MainviewActivity extends FragmentActivity implements OnPageChangeListener,OnClickListener {
 	private String username;
 	private Smack smack;
 	// three kind of the fragment to fill the content part
-	private final static int TYPE_CHATTING_FRAGMENT = 1;
-	private final static int TYPE_CONTACTS_FRAGMENT = 2;
-	private final static int TYPE_SETTING_FRAGMENT = 3;
+	private final static int TYPE_CHATTING_FRAGMENT = 0;
+	private final static int TYPE_CONTACTS_FRAGMENT = 1;
+	private final static int TYPE_SETTING_FRAGMENT = 2;
 	// three icon in the footer part
 	private ImageView footer_chatting_icon;
 	private ImageView footer_contacts_icon;
 	private ImageView footer_setting_icon;
 	// last_fragment reference to the last choosen fragment
 	// current_fragment reference to the current choosen fragment
-	private int last_fragment = 1;
-	private int current_fragment = 2;
+	private int last_fragment = 0;
+	private int current_fragment = 1;
 	private final static int LIGHT_UP = 1;
 	private final static int LIGHT_DOWN = 0;
 	// footer icon
 	private IconicFontDrawable footerChatDrawable;
 	private IconicFontDrawable footerContactsDrawable;
 	private IconicFontDrawable footerSettingsDrawable;
-	
-	//
+	//color of the three icon in the footer view32bcb6
+	private final static int LIGHT_UP_COLOR = 	Color.rgb(0xff, 0xff, 0xff);
+	private final static int LIGHT_DOWN_COLOR = Color.rgb(0x32, 0xBC, 0xcc);
+	//sum of the fragments in the viewpager
 	private final static int NUM_PAGES = 3;
-	//
+	//viewpager
 	private ViewPager vPager;
+	//
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_mainview);
-		initFooter();
-		registIconListerner();
-		updateContentFragment(TYPE_CHATTING_FRAGMENT);
+
 		vPager = (ViewPager)findViewById(R.id.mainview_pager);
 		vPager.setOffscreenPageLimit(3);
 		vPager.setAdapter(new ScreenSlidePagerAdapter(getSupportFragmentManager()));
-		
+		vPager.setOnPageChangeListener(this);
+		initFooter();
+		registIconListerner();
+		updateContentFragment(TYPE_CHATTING_FRAGMENT);
 		
 		
 		// smack = new SmackImpl();
@@ -116,25 +124,27 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 		switch (index) {
 		case TYPE_CHATTING_FRAGMENT: {
 			if (upOrDown == LIGHT_UP) {
-				footerChatDrawable.setIconColor(color.pocket_gold);
+				footerChatDrawable.setIconColor(LIGHT_UP_COLOR);
+
 			} else if (upOrDown == LIGHT_DOWN) {
-				footerChatDrawable.setIconColor(Color.WHITE);
+				footerChatDrawable.setIconColor(LIGHT_DOWN_COLOR);
+
 			}
 			break;
 		}
 		case TYPE_CONTACTS_FRAGMENT: {
 			if (upOrDown == LIGHT_UP) {
-				footerContactsDrawable.setIconColor(color.pocket_gold);
+				footerContactsDrawable.setIconColor(LIGHT_UP_COLOR);
 			} else if (upOrDown == LIGHT_DOWN) {
-				footerContactsDrawable.setIconColor(Color.WHITE);
+				footerContactsDrawable.setIconColor(LIGHT_DOWN_COLOR);
 			}
 			break;
 		}
 		case TYPE_SETTING_FRAGMENT: {
 			if (upOrDown == LIGHT_UP) {
-				footerSettingsDrawable.setIconColor(color.pocket_gold);
+				footerSettingsDrawable.setIconColor(LIGHT_UP_COLOR);
 			} else if (upOrDown == LIGHT_DOWN) {
-				footerSettingsDrawable.setIconColor(Color.WHITE);
+				footerSettingsDrawable.setIconColor(LIGHT_DOWN_COLOR);
 			}
 			break;
 		}
@@ -142,11 +152,9 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 	}
 
 	public void updateContentFragment(int fragmentType) {
-		FragmentManager fragmentManager = getSupportFragmentManager();
-		android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager
-				.beginTransaction();
 		switch (fragmentType) {
 		case TYPE_CHATTING_FRAGMENT:
+			vPager.setCurrentItem(TYPE_CHATTING_FRAGMENT);
 			last_fragment = current_fragment;
 			current_fragment = TYPE_CHATTING_FRAGMENT;
 			if (current_fragment != last_fragment) {
@@ -155,6 +163,7 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 			}
 			break;
 		case TYPE_CONTACTS_FRAGMENT:
+			vPager.setCurrentItem(TYPE_CONTACTS_FRAGMENT);
 			last_fragment = current_fragment;
 			current_fragment = TYPE_CONTACTS_FRAGMENT;
 			if(current_fragment != last_fragment){
@@ -163,6 +172,7 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 			}
 			break;
 		case TYPE_SETTING_FRAGMENT:
+			vPager.setCurrentItem(TYPE_SETTING_FRAGMENT);
 			last_fragment = current_fragment;
 			current_fragment = TYPE_SETTING_FRAGMENT;
 			if(current_fragment != last_fragment){
@@ -180,19 +190,20 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 	public void initFooter() {
 		footerChatDrawable = new IconicFontDrawable(getBaseContext());
 		footerChatDrawable.setIcon(IconicIcon.CHAT_INV);
-		footerChatDrawable.setIconColor(Color.WHITE);
+		footerChatDrawable.setIconColor(LIGHT_UP_COLOR);
 		footer_chatting_icon = (ImageView) findViewById(R.id.Footer_chattingIcon);
 		footer_chatting_icon.setBackground(footerChatDrawable);
 
+		
 		footerContactsDrawable = new IconicFontDrawable(getBaseContext());
 		footerContactsDrawable.setIcon(IconicIcon.USER);
-		footerContactsDrawable.setIconColor(Color.WHITE);
+		footerContactsDrawable.setIconColor(LIGHT_DOWN_COLOR);
 		footer_contacts_icon = (ImageView) findViewById(R.id.Footer_contactsIcon);
 		footer_contacts_icon.setBackground(footerContactsDrawable);
 
 		footerSettingsDrawable = new IconicFontDrawable(getBaseContext());
 		footerSettingsDrawable.setIcon(IconicIcon.COG);
-		footerSettingsDrawable.setIconColor(Color.WHITE);
+		footerSettingsDrawable.setIconColor(LIGHT_DOWN_COLOR);
 		footer_setting_icon = (ImageView) findViewById(R.id.Footer_settingIcon);
 		footer_setting_icon.setBackground(footerSettingsDrawable);
 	}
@@ -210,17 +221,39 @@ public class MainviewActivity extends FragmentActivity implements OnClickListene
 		switch (v.getId()) {
 		case R.id.Footer_chattingIcon:
 			updateContentFragment(TYPE_CHATTING_FRAGMENT);
-			System.out.println("fffff111111111111111111");
 			break;
 		case R.id.Footer_contactsIcon:
 			updateContentFragment(TYPE_CONTACTS_FRAGMENT);
-			System.out.println("fffff22222222222222");
 			break;
 		case R.id.Footer_settingIcon:
 			updateContentFragment(TYPE_SETTING_FRAGMENT);
-			System.out.println("fffff133333333333333");
 			break;
 		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("choosen   " + arg0);
+		last_fragment = current_fragment;
+		current_fragment = arg0;
+		if(current_fragment != last_fragment){
+			updatefooterIcon(last_fragment, LIGHT_DOWN);
+			updatefooterIcon(arg0,LIGHT_UP);
+		}
+			
 	}
 
 }
