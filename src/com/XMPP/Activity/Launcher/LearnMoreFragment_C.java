668466b4d8e5661ad95b9374a9d3f3d36c00c712
Leaking <chenhuazhaoao@gmail.com>
2014-12-14 20:54:24 +0800

    change

--- src/com/XMPP/Activity/Launcher/LearnMoreFragment_C.java
+++ src/com/XMPP/Activity/Launcher/LearnMoreFragment_C.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 import com.XMPP.R;
 
 public class LearnMoreFragment_C extends Fragment {
-
+//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.learnmore_c_fragment, container, false);
+                R.layout.learnmore_a_fragment, container, false);
 
         return rootView;
     }
--- src/com/XMPP/Activity/Launcher/LearnMoreFragment_D.java
+++ src/com/XMPP/Activity/Launcher/LearnMoreFragment_D.java
@@ -16,7 +16,7 @@ public class LearnMoreFragment_D extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
-                R.layout.learnmore_d_fragment, container, false);
+                R.layout.learnmore_a_fragment, container, false);
 
         return rootView;
     }
